4a76c510f6391eb0bfc87b705d9c2af8120d0f1a
Tommy Issa <tommyissa@hotmail.com>
2025-02-03 11:27:34 +0100

    Har skapat en variabel för att lagra kurser, eventlyssnare samt funktion vid sidans omladdning

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Variabel för att lagra kurser
+let courses = [];
+
+// Eventlyssnare vid sidans start/omstart
+window.onload = init;
+
+// Funktion vid sidans start/omstart
+function init() {
+
+
+}
+
+
